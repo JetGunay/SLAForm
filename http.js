977a4cf875277f86f36799d9f7779d5a45060292
Jet Gunay <47313684+JetGunay@users.noreply.github.com>
2023-07-04 15:24:13 +0800

    Update http.js

--- http.js
+++ http.js
@@ -7,7 +7,7 @@ const http = axios.create({
     "Access-Control-Allow-Headers": "Origin, Authorization",
     // "Access-Control-Allow-Credentials": false,
     Authorization:
-      "Bearer eyJ0dCI6InAiLCJhbGciOiJIUzI1NiIsInR2IjoiMSJ9.eyJkIjoie1wiYVwiOjM4NDYzOTMsXCJpXCI6ODU4ODc4NyxcImNcIjo0NjM4OTEzLFwidVwiOjk1OTM4MTIsXCJyXCI6XCJVU1wiLFwic1wiOltcIldcIixcIkZcIixcIklcIixcIlVcIixcIktcIixcIkNcIixcIkRcIixcIk1cIixcIkFcIixcIkxcIixcIlBcIl0sXCJ6XCI6W10sXCJ0XCI6MH0iLCJpYXQiOjE2Nzk1NTg4MTh9.e97VbvFVDYUER19J4KSlThedR71IECnZiW3eqF6pmzc",
+      "Bearer " +token,
   },
 });
 
